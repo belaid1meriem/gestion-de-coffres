16fb184cebf3bdcc29266185c3577a0584498294
belaid1meriem <mm_belaid@esi.dz>
2025-07-08 12:01:50 +0100

    Routing setted up

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import SignupView from '../views/SignupView.vue'
+
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +14,39 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView ,
+    },
+    {
+      path: '/historique',
+      name: 'historique',
+      component: () => import('../views/HistoriqueView.vue')
+    },
+    {
+      path: '/recherche',
+      name: 'recherche',
+      component: () => import('../views/SearchView.vue')
+    },
+    // {
+    //   path: '/coffre/add',
+    //   name: 'coffre-add',
+    //   component: () => import('../views/AddCoffreView.vue')
+    // },
+    // {
+    //   path: '/coffre/edit/:id',
+    //   name: 'coffre-edit',
+    //   component: () => import('../views/EditCoffreView.vue'),
+    // },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',  
+      component: () => import('../views/NotFoundView.vue')
     },
   ],
 })
